9d705048df005fd5ab89952028c3c74e7d1c5776
Jean Motte <jijimotte@gmail.com>
2024-06-26 22:43:45 +0200

    solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+  result = 0
+  if (start < 0 || end < 0) return "ERROR";
+  if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+  if (start <= end) {
+    for (i = start; i <= end; i++) {result += i}
+  } else if (start > end) {
+    for (i = end; i <= start; i++) {result += i}
+  }
+  return result
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// Think about how you would do this on pen and paper and then how you might translate that process into code:
+// - make sure you pay attention to the function parameters
+// - create a variable to hold the final sum
+// - loop through the given numbers ([link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration))
+// - on each iteration add the number to the sum
+// - return the sum after finishing the loop
