0f26cc0172e0ffc825a16947adfe0167281f9001
Jean Motte <jijimotte@gmail.com>
2024-06-26 18:31:18 +0200

    solve repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+  let sentence = ""
+  if (num >= 0) {
+    for (let i = 0; i < num; i++ ) {
+      sentence += string}
+    return sentence
+  }
+  else {
+      return "ERROR"
+    }
+  };
 
 // Do not edit below this line
 module.exports = repeatString;
