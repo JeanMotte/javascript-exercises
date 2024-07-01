6f2d42e2fb6d31d3ecb6531d9c3c2ea608ffa266
Jean Motte <jijimotte@gmail.com>
2024-07-01 23:02:44 +0200

    start defining function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // let ages = people.map((element) => element.yearOfDeath - element.yearOfBirth);
 
+  // let maxAge = Math.max(...ages)
+  // let index = ages.indexOf(maxAge)
+  // return people[index].name
+  return people
 };
 
 // Do not edit below this line
