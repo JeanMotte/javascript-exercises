f76a706e582f9d2d3858db3812b764c90a955b39
Jean Motte <jijimotte@gmail.com>
2024-07-01 22:43:07 +0200

    solve object exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object) {
+  // let result = []
+  // Object.values(object).forEach((element) => {
+  // 	result.push(element.title)
+  // })
+  // return result
+  return object.map((book) => book.title);
 };
 
 // Do not edit below this line
