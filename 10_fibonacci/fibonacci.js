1f6e7bfdc793df75dab47648623807ce4ff9aa98
Jean Motte <jijimotte@gmail.com>
2024-07-01 22:02:45 +0200

    solve fibonacci serie issue

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+  let count
+  if (typeof number !== 'number') {
+    count = parseInt(number)
+  } else {
+    count = number
+  }
+
+  if (count < 0) return "OOPS";
+  if (count === 0) return 0;
+
+  let arr = [0, 1]
+
+  for (i = 2; i <= count ; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2]
+  }
+  return arr[count]
 };
 
 // Do not edit below this line
