ed15b3844f96f4e4bdeeee52df44cb334a4c2d3f
Jean Motte <jijimotte@gmail.com>
2024-07-01 19:44:03 +0200

    solve palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+  return (String(sentence.toLowerCase().replace(/[.,?!\s]/g, '').split('').join('')) === String(sentence.toLowerCase().replace(/[.,?!\s]/g, '').split('').reverse().join(''))) ? true : false
 };
 
 // Do not edit below this line
