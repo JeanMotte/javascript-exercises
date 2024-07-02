acdf7ad2132a7207764783effcb8878278d3e350
Jean Motte <jijimotte@gmail.com>
2024-07-02 11:30:54 +0200

    solve oldest person exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,9 @@
 const findTheOldest = function(people) {
-  // let ages = people.map((element) => element.yearOfDeath - element.yearOfBirth);
+  let ages = people.map((element) => (element.yearOfDeath !== undefined) ? (element.yearOfDeath - element.yearOfBirth) : (new Date().getFullYear() - element.yearOfBirth));
 
-  // let maxAge = Math.max(...ages)
-  // let index = ages.indexOf(maxAge)
-  // return people[index].name
-  return people
+  let maxAge = Math.max(...ages)
+  let index = ages.indexOf(maxAge)
+  return people[index]
 };
 
 // Do not edit below this line
