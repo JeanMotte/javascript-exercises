aff040dcc24f76f1d4542a19ff5be3826175a480
Jean Motte <jijimotte@gmail.com>
2024-07-03 11:50:20 +0200

    start display library content

--- 15_libraryProject/lib.js
+++ 15_libraryProject/lib.js
@@ -9,10 +9,23 @@ function Book(title, author, pages, read) {
       console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${(this.read) ? 'already read' : 'not read yet'}`)
     }
   }
-}
+
 let bookOne = new Book("The Hobbit", "Tolkien", 295, true)
 myLibrary.push(bookOne)
 
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
+
+// DISPLAYING ON PAGE
+
+let divBooks = document.querySelector(".books");
+let addBookBtn = document.querySelector(".add-btn");
+
+myLibrary.forEach(book => {
+  divBooks.insertAdjacentHTML('afterbegin', `<li>${book.title}</li>`)
+});
+
+addBookBtn.addEventListener(("click"), (e) => {
+  e.preventDefault();
+})
